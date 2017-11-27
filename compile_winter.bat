41b7058b26f4778b8abf37dd8490afd9f180fdb9
Brian Lach <brianlach72@gmail.com>
2017-11-26 20:14:00 -0500

    Updated dc and compile_winter.bat

--- compile_winter.bat
+++ compile_winter.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Compiling winter.mf...
 cd winter
-multify -c -f ..\winter.mf winter phase_3.5 phase_4 phase_6
+..\..\..\Panda3D-CI\bin\multify -c -f ..\winter.mf winter phase_3.5 phase_4 phase_6
 echo Done!
 PAUSE
