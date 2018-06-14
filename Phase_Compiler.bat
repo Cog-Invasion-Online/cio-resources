f2fb02a05c45511b204237ef2d67ad3921e06888
Brian Lach <brianlach72@gmail.com>
2018-06-14 13:30:23 -0400

    fix paths to new panda directory

--- Phase_Compiler.bat
+++ Phase_Compiler.bat
@@ -2,7 +2,7 @@
 title Cog Invasion Phase File Compiler
 
 set /p file=What phase file do you want compiled? 
-..\..\Panda3D-CI\python\ppython.exe -B tool_Multifile.py --mtype compile --filename %file%
+..\..\cio-panda3d\built_x64\python\ppython.exe -B tool_Multifile.py --mtype compile --filename %file%
 
 echo Done!
 pause >nul
--- compile_winter.bat
+++ compile_winter.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Compiling winter.mf...
 cd winter
-..\..\..\Panda3D-CI\bin\multify -c -f ..\winter.mf winter phase_3.5 phase_4 phase_6
+..\..\..\cio-panda3d\built_x64\bin\multify -c -f ..\winter.mf winter phase_3.5 phase_4 phase_6
 echo Done!
 PAUSE
