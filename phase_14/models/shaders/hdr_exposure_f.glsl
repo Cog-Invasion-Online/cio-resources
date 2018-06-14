d0a41c627484280767a8c78210c07e88905c09bd
Brian Lach <brianlach72@gmail.com>
2018-06-14 11:21:25 -0400

    phase_14: add all of the new assets (too many to list off, includes the gag gui though)

---
+++ phase_14/models/shaders/hdr_exposure_f.glsl
@@ -0,0 +1,14 @@
+/**
+ * COG INVASION ONLINE
+ * Copyright (c) CIO Team. All rights reserved.
+ *
+ * @file hdr_exposure_v.glsl
+ * @author Brian Lach
+ * @date May 30, 2018
+ *
+ * @desc Determines exposure level of scene.
+ */
+ 
+//GLSL
+
+#version 140
---
+++ phase_14/models/shaders/hdr_exposure_v.glsl
@@ -0,0 +1,26 @@
+/**
+ * COG INVASION ONLINE
+ * Copyright (c) CIO Team. All rights reserved.
+ *
+ * @file hdr_exposure_v.glsl
+ * @author Brian Lach
+ * @date May 30, 2018
+ *
+ * @desc Determines exposure level of scene.
+ */
+ 
+//GLSL
+
+#version 140
+
+uniform mat4 p3d_ModelViewProjectionMatrix;
+in vec4 p3d_Vertex;
+in vec2 p3d_MultiTexCoord0;
+
+out vec2 uv;
+
+void main()
+{
+    gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
+    uv = p3d_MultiTexCoord0;
+}
