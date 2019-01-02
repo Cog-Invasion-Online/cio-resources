3d881f3ffcbe2763c3f07dcaf4321b53e6d8d397
Brian Lach <brianlach72@gmail.com>
2019-01-02 12:42:28 -0500

    add all of the raw_dna files, replace textures with materials, script to easily compile dna to pdna

---
+++ raw_dna/compile_dna.bat
@@ -0,0 +1,12 @@
+@echo off
+
+IF NOT EXIST "..\..\..\libpandadnas" (goto missinglpd) ELSE (goto compile)
+
+:missinglpd
+echo You need to clone libpandadna into the main repository directory first (the folder that has 'game' and 'cio-panda3d' in it).
+pause
+exit
+
+:compile
+..\..\..\cio-panda3d\built_x64\python\ppython -B ../../../libpandadna/compiler/compile.py phase_3.5/dna/*.dna phase_4/dna/*.dna phase_5/dna/*dna phase_5.5/dna/*.dna phase_6/dna/*.dna phase_8/dna/*.dna phase_9/dna/*.dna phase_10/dna/*.dna phase_11/dna/*.dna phase_12/dna/*.dna phase_13/dna/*.dna
+pause
