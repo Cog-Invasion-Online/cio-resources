b4ed2f0f4ac1638cee638f156d19833c298f007a
Brian Lach <brianlach72@gmail.com>
2019-01-10 21:00:45 -0500

    bamify: specify per mesh material file, run egg-trans (for tangent+binormals), optimize option

--- bamify.py
+++ bamify.py
@@ -1,14 +1,57 @@
 from panda3d.core import *
 from panda3d.bsp import *
 
+import os
+
+def lbr():
+    print("\n====================================================\n")
+
 inp_file = raw_input("Input file: ")
 out_file = raw_input("Output file: ")
-mat_file = raw_input("Material file: ")
+
+lbr()
+
+print("Running egg-trans...")
+cmd  = "..\\..\\cio-panda3d\\built_x64\\bin\\egg-trans -o {0} -tbnall ".format(inp_file)
+cmd += inp_file
+os.system(cmd)
+        
+lbr()
 
 loader = Loader.getGlobalPtr()
 node = loader.loadSync(inp_file)
 np = NodePath(node)
-np.setAttrib(BSPMaterialAttrib.make(BSPMaterial.getFromFile(mat_file)))
-np.clearModelNodes()
-np.flattenStrong()
+
+meshes = np.findAllMatches("**/+GeomNode")
+for meshNp in meshes:
+    mat_file = raw_input("Material file for mesh `{0}`: ".format(meshNp.getName()))
+    meshNp.setAttrib(BSPMaterialAttrib.make(BSPMaterial.getFromFile(mat_file)))
+    
+lbr()
+    
+optimizeStr = ""
+optimize = False
+while (optimizeStr not in ["y", "n"]):
+    optimizeStr = raw_input("Optimize model? (highly recommended) [Y/N]: ").lower()
+    if optimizeStr == "y":
+        optimize = True
+    elif optimizeStr == "n":
+        optimize = False
+        
+lbr()
+
+if optimize:
+    print("\nModel before optimization:")
+    np.ls()
+    
+    np.clearModelNodes()
+    np.flattenStrong()
+    
+    print("\nModel after optimization:")
+    np.ls()
+    print
+    
+lbr()
+
+print("Writing {0}...".format(out_file))
 np.writeBamFile(out_file)
--- bamify_run.bat
+++ bamify_run.bat
@@ -1,2 +1,4 @@
+@echo off
+
 ..\..\cio-panda3d\built_x64\python\ppython -B bamify.py
 pause
