741c45b1fee1d35a5f9f76c6510831814c3e2871
Brian Lach <brianlach72@gmail.com>
2019-03-16 22:56:56 -0400

    phase_14: optimize HDR build_histogram shader

--- phase_14/models/shaders/build_histogram.compute.glsl
+++ phase_14/models/shaders/build_histogram.compute.glsl
@@ -1,25 +1,13 @@
 #version 430
 
-layout(local_size_x = 16, local_size_y = 16) in;
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 16) in;
 
 uniform sampler2D scene_texture;
 layout(r32i) uniform iimage1D histogram_texture;
 
 uniform vec2 bucketrange[16];
 
-int getBucketFromLuminance(float luminance)
-{
-    for (int i = 0; i < 16; i++)
-    {
-        if (luminance >= bucketrange[i].x && luminance < bucketrange[i].y)
-        {
-            // luminance falls into range of this bucket.
-            return i;
-        }
-    }
-    
-    return -1;
-}
+const vec3 luminance_weights = vec3( 0.2125f, 0.7154f, 0.0721f );
 
 void main()
 {
@@ -28,11 +16,12 @@ void main()
     // Extract color from scene.
     vec3 color = texelFetch(scene_texture, coords, 0).rgb;
     // Convert to luminance.
-    float lum = ( 0.2125f * color.r ) + ( 0.7154f * color.g ) + ( 0.0721f * color.b );
+    float luminance = dot(color, luminance_weights);
     
-    int bucket = getBucketFromLuminance(lum);
-    if (bucket >= 0)
+    int i = int(gl_LocalInvocationID.z);
+    if (luminance >= bucketrange[i].x && luminance < bucketrange[i].y)
     {
-        imageAtomicAdd(histogram_texture, bucket, 1);
+        // luminance falls into range of this bucket.
+        imageAtomicAdd(histogram_texture, i, 1);
     }
 }
