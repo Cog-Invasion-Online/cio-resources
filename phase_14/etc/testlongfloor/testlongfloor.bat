4249f4c928e40a53419a2200ee8ea8e7d1ff69a9
Brian Lach <brianlach72@gmail.com>
2019-03-26 18:33:30 -0400

    phase_14: recompile some maps for the ambient fix phase_3.5: setup PBR materials and normals for sellbot suit

--- phase_14/etc/testlongfloor/testlongfloor.bat
+++ phase_14/etc/testlongfloor/testlongfloor.bat
@@ -1,6 +1,6 @@
 cd "D:\OTHER\lachb\Documents\cio\game\resources\phase_14\etc\testlongfloor"
-"D:\OTHER\lachb\Documents\cio\cio-panda3d\built_x64\bin\p3csg.exe" -threads 8 "D:\OTHER\lachb\Documents\cio\game\resources\phase_14\etc\testlongfloor\testlongfloor.map"
+"D:\OTHER\lachb\Documents\cio\cio-panda3d\built_x64\bin\p3csg.exe" -mfdir /d/OTHER/lachb/Documents/cio/game/resources -threads 8 "D:\OTHER\lachb\Documents\cio\game\resources\phase_14\etc\testlongfloor\testlongfloor.map"
 "D:\OTHER\lachb\Documents\cio\cio-panda3d\built_x64\bin\p3bsp.exe" -threads 8 "D:\OTHER\lachb\Documents\cio\game\resources\phase_14\etc\testlongfloor\testlongfloor.map"
 "D:\OTHER\lachb\Documents\cio\cio-panda3d\built_x64\bin\p3vis.exe" -full  -threads 8 "D:\OTHER\lachb\Documents\cio\game\resources\phase_14\etc\testlongfloor\testlongfloor.map"
-"D:\OTHER\lachb\Documents\cio\cio-panda3d\built_x64\bin\p3rad.exe" -final -extra -mfincludefile "D:\\OTHER\\lachb\\Documents\\cio\\game\\resources\\phase_14\\etc\\rad_mfinclude.txt" -threads 8 "D:\OTHER\lachb\Documents\cio\game\resources\phase_14\etc\testlongfloor\testlongfloor.map"
+"D:\OTHER\lachb\Documents\cio\cio-panda3d\built_x64\bin\p3rad.exe" -final -extra -mfdir /d/OTHER/lachb/Documents/cio/game/resources -threads 8 "D:\OTHER\lachb\Documents\cio\game\resources\phase_14\etc\testlongfloor\testlongfloor.map"
 pause
